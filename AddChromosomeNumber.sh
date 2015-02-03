bc8ff75fa4edac69d47849cffbc59fd05b2429d1
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-02-03 13:56:44 +0000

    Create AddChromosomeNumber.sh
    Post-imputation clean-up script to add chromosome number to by-chromosome .impute2 files

---
+++ AddChromosomeNumber.sh
@@ -0,0 +1,5 @@
+for i in {1..22}
+do
+awk -v i=$i {s=""; for (j=2; j <= NF; j++) s=s $j " "; print i, s}' path/to/results-directory/Chr$i.impute2 > New_Chromosome$i.impute2
+done
+awk ' NR==1 {print; next}{ s = ""; for (j =2; j <= NF; j++) s = s $j " "; print "X", s }' path/to/results-directory/ChrX.impute2 > New_ChromosomeX.impute2
