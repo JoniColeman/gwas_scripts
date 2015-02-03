e5899237b413ada475b5b3ad95aae6d8f146a89c
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-02-03 14:07:11 +0000

    Create ManhattanPlotinR.R
    Wrapper script for running Mike Weale's manhattan_v1.R script to generate Manhattan plots from association results

---
+++ ManhattanPlotinR.R
@@ -0,0 +1,7 @@
+setwd("/path/to/data")
+source("manhattan_v1.R")
+gwas1 <- read.table("dataname_post_imputation_final_analysis_FOR_MP",head=T)
+data_to_plot <- data.frame(CHR=gwas1$CHR, BP=gwas1$BP, P=gwas1$P)
+pdf("dataname_post_imputation_final_analysis_MP.pdf",width=8,height=6)
+manhattan(data_to_plot, GWthresh=5e-8, GreyZoneThresh=1e-4, DrawGWline=T)
+dev.off() 
