be64c367fe85d9aad189b21594882e72a8fd967b
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-02-03 13:59:42 +0000

    Create FilterByInfoAll.sh
    Post-imputation QC script to filter whole-genome impute2_info file by info score, and then keep only these variants from the by-chromosome impute2_info files

---
+++ FilterByInfoAll.sh
@@ -0,0 +1,8 @@
+gunzip dataname_whole_genome.impute2_info.gz
+awk '$5 >= 0.8' dataname_whole_genome.impute2_info > dataname_whole_genome_filtered.impute2_info
+gzip dataname_whole_genome.impute2_info
+for i in {1..22}
+do
+awk 'FNR==NR { a[$2]; next } $2 in a' dataname_whole_genome_filtered.impute2_info New_Chromosome$i.impute2 > Filtered_Chromsome$i.impute2
+done
+awk 'FNR==NR { a[$2]; next } $2 in a' dataname_whole_genome_filtered.impute2_info New_ChromosomeX.impute2 > Filtered_ChromsomeX.impute2
