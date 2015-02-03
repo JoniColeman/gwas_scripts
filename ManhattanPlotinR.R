66f34feda0232eea1d97744384f0b58ff6baba3b
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-02-03 14:26:51 +0000

    Update ManhattanPlotinR.R
    Updated to manhattan_v2.R

--- ManhattanPlotinR.R
+++ ManhattanPlotinR.R
@@ -1,5 +1,5 @@
 setwd("/path/to/data")
-source("manhattan_v1.R")
+source("manhattan_v2.R")
 gwas1 <- read.table("dataname_post_imputation_final_analysis_FOR_MP",head=T)
 data_to_plot <- data.frame(CHR=gwas1$CHR, BP=gwas1$BP, P=gwas1$P)
 pdf("dataname_post_imputation_final_analysis_MP.pdf",width=8,height=6)
