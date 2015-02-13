0b2eb0f5d633fe3223821967b9f433a44fd1d798
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-02-13 15:16:16 +0000

    Update DropDuplicatedPositions.sh

--- DropDuplicatedPositions.sh
+++ DropDuplicatedPositions.sh
@@ -8,3 +8,6 @@ uniq –d > More_Duplicates_Removed
 --make-bed \
 --out dataname_post_imputation_final
 
+awk 'BEGIN {OFS = "\t"} $2 ~ /^rs/{gsub(":.*", "", $2) }1' dataname_post_imputation_final.bim > dataname_post_imputation_final_rs_only.bim
+cp dataname_post_imputation_final.bed dataname_post_imputation_final_rs_only.bed
+cp dataname_post_imputation_final.fam dataname_post_imputation_final_rs_only.fam
