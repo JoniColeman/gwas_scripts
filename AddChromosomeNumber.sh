e535066b3820175a93083bf9c2661220e1a82ad0
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-06-26 11:21:10 +0100

    Provide user input
    Allow user to specify path to results directory as part of the command to script.

--- AddChromosomeNumber.sh
+++ AddChromosomeNumber.sh
@@ -1,5 +1,6 @@
+results=$1
 for i in {1..22}
 do
-awk -v i=$i '{s=""; for (j=2; j <= NF; j++) s=s $j " "; print i, s}' path/to/results-directory/Chr$i.impute2 > New_Chromosome$i.impute2
+awk -v i=$i results=$results '{s=""; for (j=2; j <= NF; j++) s=s $j " "; print i, s}' $results/Chr$i.impute2 > New_Chromosome$i.impute2
 done
-awk ' NR==1 {print; next}{ s = ""; for (j =2; j <= NF; j++) s = s $j " "; print "X", s }' path/to/results-directory/ChrX.impute2 > New_ChromosomeX.impute2
+awk -v results=$results ' NR==1 {print; next}{ s = ""; for (j =2; j <= NF; j++) s = s $j " "; print "X", s }' $results/ChrX.impute2 > New_ChromosomeX.impute2
