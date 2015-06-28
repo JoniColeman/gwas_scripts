19733ca4f63b5c5f347ff259c8140b4b9f01da29
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-06-28 13:35:58 +0100

    Added root

--- DropDuplicatedPositions.sh
+++ DropDuplicatedPositions.sh
@@ -1,13 +1,13 @@
-awk '{print $2}' dataname_post_imputation_final_rs_only.bim | \
+awk '{print $2}' $root.post_imputation_final_rs_only.bim | \
 sort | \ 
 uniq –d > More_Duplicates_Removed
 
 ./plink2 \
---bfile dataname_post_imputation_final \
+--bfile $root.post_imputation_final \
 --exclude More_Duplicates_Removed \
 --make-bed \
---out dataname_post_imputation_final
+--out $root.post_imputation_final
 
-awk 'BEGIN {OFS = "\t"} $2 ~ /^rs/{gsub(":.*", "", $2) }1' dataname_post_imputation_final.bim > dataname_post_imputation_final_rs_only.bim
-cp dataname_post_imputation_final.bed dataname_post_imputation_final_rs_only.bed
-cp dataname_post_imputation_final.fam dataname_post_imputation_final_rs_only.fam
+awk 'BEGIN {OFS = "\t"} $2 ~ /^rs/{gsub(":.*", "", $2) }1' $root.post_imputation_final.bim > $root.post_imputation_final_rs_only.bim
+cp $root.post_imputation_final.bed $root.post_imputation_final_rs_only.bed
+cp $root.post_imputation_final.fam $root.post_imputation_final_rs_only.fam
