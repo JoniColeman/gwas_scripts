4115a814440b74079b63d22f8e9f95ddc035c21f
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-06-28 13:46:52 +0100

    Update FilterByInfoAll.sh

--- FilterByInfoAll.sh
+++ FilterByInfoAll.sh
@@ -1,8 +1,8 @@
-gunzip dataname_whole_genome.impute2_info.gz
-awk '$5 >= 0.8' dataname_whole_genome.impute2_info > dataname_whole_genome_filtered.impute2_info
-gzip dataname_whole_genome.impute2_info
+gunzip $root.whole_genome.impute2_info.gz
+awk '$5 >= 0.8' $root.whole_genome.impute2_info > $root.whole_genome_filtered.impute2_info
+gzip $root.whole_genome.impute2_info
 for i in {1..22}
 do
-awk 'FNR==NR { a[$2]; next } $2 in a' dataname_whole_genome_filtered.impute2_info New_Chromosome$i.impute2 > Filtered_Chromsome$i.impute2
+awk 'FNR==NR { a[$2]; next } $2 in a' $root.whole_genome_filtered.impute2_info New_Chromosome$i.impute2 > Filtered_Chromsome$i.impute2
 done
-awk 'FNR==NR { a[$2]; next } $2 in a' dataname_whole_genome_filtered.impute2_info New_ChromosomeX.impute2 > Filtered_ChromsomeX.impute2
+awk 'FNR==NR { a[$2]; next } $2 in a' $root.whole_genome_filtered.impute2_info New_ChromosomeX.impute2 > Filtered_ChromsomeX.impute2
