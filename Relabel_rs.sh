781d46c178db3ff151bf6cc7f3eb338718451dfa
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-07-06 18:35:29 +0100

    Update Relabel_rs.sh

--- Relabel_rs.sh
+++ Relabel_rs.sh
@@ -1,3 +1,5 @@
+source Config.conf
+
 awk 'BEGIN {OFS = "\t"} $2 ~ /^rs/{gsub(":.*", "", $2) }1' $root.post_imputation_final.bim > $root.post_imputation_final_rs_only.bim
 cp $root.post_imputation_final.bed $root.post_imputation_final_rs_only.bed
 cp $root.post_imputation_final.fam $root.post_imputation_final_rs_only.fam
