c70275801528dedb23a6c0903690627afc605dba
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-07-06 19:48:14 +0100

    Update ManhattanPlotinR.R

--- ManhattanPlotinR.R
+++ ManhattanPlotinR.R
@@ -1,5 +1,6 @@
 source("manhattan_v2.R")
-root<-$1
+args <- commandArgs(TRUE)
+root <- args[1]
 gwas1 <- read.table(paste(root,".post_imputation_final_analysis_FOR_MP",sep=""),head=T)
 data_to_plot <- data.frame(CHR=gwas1$CHR, BP=gwas1$BP, P=gwas1$P)
 pdf(paste(root,".post_imputation_final_analysis_MP.pdf",sep=""),width=8,height=6)
