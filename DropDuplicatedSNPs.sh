d32a64f266be0e880676c4648957ee0eedee9ab3
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-07-06 18:31:52 +0100

    Update DropDuplicatedSNPs.sh

--- DropDuplicatedSNPs.sh
+++ DropDuplicatedSNPs.sh
@@ -1,3 +1,5 @@
+source Config.conf
+
 awk '{print $0, $1":"$4}' $root.post_imputation_updated.bim > $root.post_imputation_updated_positions
 awk '{print $1":"$4}' $root.post_imputation_updated.bim | sort | uniq -d | $root.post_imputation_updated_duplicated_positions 
 grep -w -f  $root.post_imputation_updated_duplicated_positions $root.post_imputation_updated_positions | awk ‘{print $2}’ > $root.post_imputation_updated_duplicated_IDs
