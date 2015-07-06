077203f1eff837fc3ba09b66ca7ffeb14403fba4
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-07-06 19:59:09 +0100

    Update PC-VS-OUTCOME_IN_R_FULL.R

--- PC-VS-OUTCOME_IN_R_FULL.R
+++ PC-VS-OUTCOME_IN_R_FULL.R
@@ -4,7 +4,7 @@ pheno <- args[2]
 PCAEVEC<-read.table(paste(root,".pca.evec",sep=""),head=T)
 PHENOTYPE<-read.table(pheno,head=T)
 PCAPHENO<-merge(PCAEVEC,PHENOTYPE)
-sink("path/to/PC_Output_Associations.txt")
+sink(paste(root,"PC_Output_Associations.txt",sep=""))
 for (i in 1:100) {
 DATA<-PCAPHENO[,c(3:(i+2),103)]
 print(summary(lm(Outcome ~ ., DATA)))
