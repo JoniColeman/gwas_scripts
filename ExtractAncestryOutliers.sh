d02617ca1d4b5d4b8bfcc751ed9c948242527b59
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2015-07-07 11:23:17 +0100

    Update ExtractAncestryOutliers.sh

--- ExtractAncestryOutliers.sh
+++ ExtractAncestryOutliers.sh
@@ -1,3 +1,3 @@
 source Config.conf
 
-awk '{print $3}' <(grep -w REMOVED $root.pop_strat_outliers_smartpca.log) | sed 's/:/ /g' > $root._pop_strat_outliers.outliers 
+awk '{print $3}' $(grep -w REMOVED $root.pop_strat_outliers_smartpca.log) | sed 's/:/ /g' > $root._pop_strat_outliers.outliers 
