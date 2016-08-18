85be52dc69401da234edb495edf59b88f27b52ae
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2016-08-18 15:32:06 +0100

    Added support for always uppercase alleles
    Force all alleles to be uppercase in output, even if lowercase in input.
    Also corrected case error in hg18 reference and prettified the output.

--- ID_Build.py
+++ ID_Build.py
@@ -2,7 +2,6 @@
 
 ##Basic build identifier
 ##Run as ID_Build.py PLINK_CHROMSOME_6_FILE.bim
-##Recommendation - run binary through plink before using this script - sets A1 as minor allele
 
 import sys
 import numpy as np
@@ -26,7 +25,7 @@ def main():
                              '116083117' : 'T',
                              '17839694' : 'T',
                              '11943869' : 'G',
-                             '105624138' : 't',
+                             '105624138' : 'T',
                              '130013753' : 'G',
                              '168776041' : 'T',
                              '168722477' : 'X',
@@ -113,135 +112,135 @@ def main():
                              '168633436' : 'T'}
             bim_6_df = pd.read_table(bim_6_file, delim_whitespace=True, header=None, prefix='V', index_col=0, usecols=[3,4])
             if np.sum(bim_6_df.index.isin([24979336])) == 1:
-                bim_6_dict = {'24979336' : bim_6_df.loc[24979336,'V4']}
+                bim_6_dict = {'24979336' : bim_6_df.loc[24979336,'V4'].upper()}
             else:
                 bim_6_dict = {'24979336' : 'X'}
             if np.sum(bim_6_df.index.isin([32537182])) == 1:
-                bim_6_dict['32537182'] = bim_6_df.loc[32537182,'V4']
+                bim_6_dict['32537182'] = bim_6_df.loc[32537182,'V4'].upper()
             else:
                 bim_6_dict['32537182'] = 'X'
             if np.sum(bim_6_df.index.isin([32647698])) == 1:
-                bim_6_dict['32647698'] = bim_6_df.loc[32647698, 'V4']
+                bim_6_dict['32647698'] = bim_6_df.loc[32647698, 'V4'].upper()
             else:
                 bim_6_dict['32647698'] = 'X'
             if np.sum(bim_6_df.index.isin([24871357])) == 1:
-                bim_6_dict['24871357'] = bim_6_df.loc[24871357, 'V4']
+                bim_6_dict['24871357'] = bim_6_df.loc[24871357, 'V4'].upper()
             else:
                 bim_6_dict['24871357'] = 'X'
             if np.sum(bim_6_df.index.isin([32429204])) == 1:
-                bim_6_dict['32429204'] = bim_6_df.loc[32429204, 'V4']
+                bim_6_dict['32429204'] = bim_6_df.loc[32429204, 'V4'].upper()
             else:
                 bim_6_dict['32429204'] = 'X'
             if np.sum(bim_6_df.index.isin([32539620])) == 1:
-                bim_6_dict['32539620'] = bim_6_df.loc[32539620, 'V4']
+                bim_6_dict['32539620'] = bim_6_df.loc[32539620, 'V4'].upper()
             else:
                 bim_6_dict['32539620'] = 'X'
             if np.sum(bim_6_df.index.isin([24871129])) == 1:
-                bim_6_dict['24871129'] = bim_6_df.loc[24871129, 'V4']
+                bim_6_dict['24871129'] = bim_6_df.loc[24871129, 'V4'].upper()
             else:
                 bim_6_dict['24871129'] = 'X'
             if np.sum(bim_6_df.index.isin([32461427])) == 1:
-                bim_6_dict['32461427'] = bim_6_df.loc[32461427, 'V4']
+                bim_6_dict['32461427'] = bim_6_df.loc[32461427, 'V4'].upper()
             else:
                 bim_6_dict['32461427'] = 'X'
             if np.sum(bim_6_df.index.isin([32571843])) == 1:
-                bim_6_dict['32571843'] = bim_6_df.loc[32571843, 'V4']
+                bim_6_dict['32571843'] = bim_6_df.loc[32571843, 'V4'].upper()
             else:
                 bim_6_dict['32571843'] = 'X'
             if np.sum(bim_6_df.index.isin([168465326])) == 1:
-                bim_6_dict['168465326'] = bim_6_df.loc[168465326, 'V4']
+                bim_6_dict['168465326'] = bim_6_df.loc[168465326, 'V4'].upper()
             else:
                 bim_6_dict['168465326'] = 'X'
             if np.sum(bim_6_df.index.isin([134105573])) == 1:
-                bim_6_dict['134105573'] = bim_6_df.loc[134105573, 'V4']
+                bim_6_dict['134105573'] = bim_6_df.loc[134105573, 'V4'].upper()
             else:
                 bim_6_dict['134105573'] = 'X'
             if np.sum(bim_6_df.index.isin([116083117])) == 1:
-                bim_6_dict['116083117'] = bim_6_df.loc[116083117, 'V4']
+                bim_6_dict['116083117'] = bim_6_df.loc[116083117, 'V4'].upper()
             else:
                 bim_6_dict['116083117'] = 'X'
             if np.sum(bim_6_df.index.isin([17839694])) == 1:
-                bim_6_dict['17839694'] = bim_6_df.loc[17839694, 'V4']
+                bim_6_dict['17839694'] = bim_6_df.loc[17839694, 'V4'].upper()
             else:
                 bim_6_dict['17839694'] = 'X'
             if np.sum(bim_6_df.index.isin([11943869])) == 1:
-                bim_6_dict['11943869'] = bim_6_df.loc[11943869, 'V4']
+                bim_6_dict['11943869'] = bim_6_df.loc[11943869, 'V4'].upper()
             else:
                 bim_6_dict['11943869'] = 'X'
             if np.sum(bim_6_df.index.isin([105624138])) == 1:
-                bim_6_dict['105624138'] = bim_6_df.loc[105624138, 'V4']
+                bim_6_dict['105624138'] = bim_6_df.loc[105624138, 'V4'].upper()
             else:
                 bim_6_dict['105624138'] = 'X'
             if np.sum(bim_6_df.index.isin([130013753])) == 1:
-                bim_6_dict['130013753'] = bim_6_df.loc[130013753, 'V4']
+                bim_6_dict['130013753'] = bim_6_df.loc[130013753, 'V4'].upper()
             else:
                 bim_6_dict['130013753'] = 'X'
             if np.sum(bim_6_df.index.isin([168776041])) == 1:
-                bim_6_dict['168776041'] = bim_6_df.loc[168776041, 'V4']
+                bim_6_dict['168776041'] = bim_6_df.loc[168776041, 'V4'].upper()
             else:
                 bim_6_dict['168776041'] = 'X'
             if np.sum(bim_6_df.index.isin([168722477])) == 1:
-                bim_6_dict['168722477'] = bim_6_df.loc[168722477, 'V4']
+                bim_6_dict['168722477'] = bim_6_df.loc[168722477, 'V4'].upper()
             else:
                 bim_6_dict['168722477'] = 'X'
             if np.sum(bim_6_df.index.isin([134063880])) == 1:
-                bim_6_dict['134063880'] = bim_6_df.loc[134063880, 'V4']
+                bim_6_dict['134063880'] = bim_6_df.loc[134063880, 'V4'].upper()
             else:
                 bim_6_dict['134063880'] = 'X'
             if np.sum(bim_6_df.index.isin([115976424])) == 1:
-                bim_6_dict['115976424'] = bim_6_df.loc[115976424, 'V4']
+                bim_6_dict['115976424'] = bim_6_df.loc[115976424, 'V4'].upper()
             else:
                 bim_6_dict['115976424'] = 'X'
             if np.sum(bim_6_df.index.isin([17731715])) == 1:
-                bim_6_dict['17731715'] = bim_6_df.loc[17731715, 'V4']
+                bim_6_dict['17731715'] = bim_6_df.loc[17731715, 'V4'].upper()
             else:
                 bim_6_dict['17731715'] = 'X'
             if np.sum(bim_6_df.index.isin([11834883])) == 1:
-                bim_6_dict['11834883'] = bim_6_df.loc[11834883, 'V4']
+                bim_6_dict['11834883'] = bim_6_df.loc[11834883, 'V4'].upper()
             else:
                 bim_6_dict['11834883'] = 'X'
             if np.sum(bim_6_df.index.isin([105517445])) == 1:
-                bim_6_dict['105517445'] = bim_6_df.loc[105517445, 'V4']
+                bim_6_dict['105517445'] = bim_6_df.loc[105517445, 'V4'].upper()
             else:
                 bim_6_dict['105517445'] = 'X'
             if np.sum(bim_6_df.index.isin([129972060])) == 1:
-                bim_6_dict['129972060'] = bim_6_df.loc[129972060, 'V4']
+                bim_6_dict['129972060'] = bim_6_df.loc[129972060, 'V4'].upper()
             else:
                 bim_6_dict['129972060'] = 'X'
             if np.sum(bim_6_df.index.isin([169034116])) == 1:
-                bim_6_dict['169034116'] = bim_6_df.loc[169034116, 'V4']
+                bim_6_dict['169034116'] = bim_6_df.loc[169034116, 'V4'].upper()
             else:
                 bim_6_dict['169034116'] = 'X'
             if np.sum(bim_6_df.index.isin([168321797])) == 1:
-                bim_6_dict['168321797'] = bim_6_df.loc[168321797, 'V4']
+                bim_6_dict['168321797'] = bim_6_df.loc[168321797, 'V4'].upper()
             else:
                 bim_6_dict['168321797'] = 'X'
             if np.sum(bim_6_df.index.isin([133742742])) == 1:
-                bim_6_dict['133742742'] = bim_6_df.loc[133742742, 'V4']
+                bim_6_dict['133742742'] = bim_6_df.loc[133742742, 'V4'].upper()
             else:
                 bim_6_dict['133742742'] = 'X'
             if np.sum(bim_6_df.index.isin([115655260])) == 1:
-                bim_6_dict['115655260'] = bim_6_df.loc[115655260, 'V4']
+                bim_6_dict['115655260'] = bim_6_df.loc[115655260, 'V4'].upper()
             else:
                 bim_6_dict['115655260'] = 'X'
             if np.sum(bim_6_df.index.isin([17731484])) == 1:
-                bim_6_dict['17731484'] = bim_6_df.loc[17731484, 'V4']
+                bim_6_dict['17731484'] = bim_6_df.loc[17731484, 'V4'].upper()
             else:
                 bim_6_dict['17731484'] = 'X'
             if np.sum(bim_6_df.index.isin([11834650])) == 1:
-                bim_6_dict['11834650'] = bim_6_df.loc[11834650, 'V4']
+                bim_6_dict['11834650'] = bim_6_df.loc[11834650, 'V4'].upper()
             else:
                 bim_6_dict['11834650'] = 'X'
             if np.sum(bim_6_df.index.isin([105069570])) == 1:
-                bim_6_dict['105069570'] = bim_6_df.loc[105069570, 'V4']
+                bim_6_dict['105069570'] = bim_6_df.loc[105069570, 'V4'].upper()
             else:
                 bim_6_dict['105069570'] = 'X'
             if np.sum(bim_6_df.index.isin([129650915])) == 1:
-                bim_6_dict['129650915'] = bim_6_df.loc[129650915, 'V4']
+                bim_6_dict['129650915'] = bim_6_df.loc[129650915, 'V4'].upper()
             else:
                 bim_6_dict['129650915'] = 'X'
             if np.sum(bim_6_df.index.isin([168633436])) == 1:
-                bim_6_dict['168633436'] = bim_6_df.loc[168633436, 'V4']
+                bim_6_dict['168633436'] = bim_6_df.loc[168633436, 'V4'].upper()
             else:
                 bim_6_dict['168633436'] = 'X'
             hg_18_score = 0
@@ -299,24 +298,15 @@ def main():
                     print 'Probable build is hg38'
                 if hg_18_score >= hg_38_score:
                     print 'Cannot determine build'
-            print '\nhg18 reference:'
-            print hg_18_ref
-            print 'Test:'
-            print Test
-            print 'Match:'
-            print hg_18_score, 'out of', hg_18_length
-            print '\nhg19 reference:'
-            print hg_19_ref
-            print 'Test:'
-            print Test
-            print 'Match:'
-            print hg_19_score, 'out of', hg_19_length
-            print '\nhg38 reference:'
-            print hg_38_ref
-            print 'Test:'
-            print Test
-            print 'Match:'
-            print hg_38_score, 'out of', hg_38_length
+            print '\nhg18 reference:', hg_18_ref
+            print 'Test:', Test
+            print 'Match:', hg_18_score, 'out of', hg_18_length
+            print '\nhg19 reference:', hg_19_ref
+            print 'Test:', Test
+            print 'Match:', hg_19_score, 'out of', hg_19_length
+            print '\nhg38 reference:', hg_38_ref
+            print 'Test:', Test
+            print 'Match:', hg_38_score, 'out of', hg_38_length
     else:
         print 'Give me a PLINK .bim file please. Chromosome 6, not pruned for preference'
 
