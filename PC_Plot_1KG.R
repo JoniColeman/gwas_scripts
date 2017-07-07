026e8ae8c1065832f82c19c9bef1994334696bd8
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2017-07-07 16:39:57 +0100

    Added rename header
    H/T Saskia Selzam

--- PC_Plot_1KG.R
+++ PC_Plot_1KG.R
@@ -1,6 +1,7 @@
 args <- commandArgs(TRUE)
 root <- args[1]
 PCAEVEC<-read.table(paste(root,".1kg.LD_pop_strat.pca.evec_RENAMED",sep=""), head=T)
+colnames(PCAEVEC) <- c("ID","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10","Pop")
 library(ggplot2)
 pdf(paste(root,"1kg.LD_pop_strat_PCA.pdf",sep=""))
 with(PCAEVEC, qplot(PC1,PC2,colour=Pop))
