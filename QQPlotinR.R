7d661edc2d1bf3339c12ef455e86699776975316
Joni Coleman <jonathan.coleman@kcl.ac.uk>
2022-05-26 15:32:23 +0100

    Update QQPlotinR.R
    Fix error in loading in QQ plot data

--- QQPlotinR.R
+++ QQPlotinR.R
@@ -1,7 +1,7 @@
 source("qq_plot_v7.R")
 args <- commandArgs(TRUE)
 root <- args[1]
-gwas1<-read.table("$root.post_imputation_final_analysis.assoc.logistic",head=T)
+gwas1<-read.table(paste(root,".post_imputation_final_analysis_p",sep=""), head=T)
 x1<-gwas1$P
 pdf(paste(root,".post_imputation_final_analysis_QQ.pdf",sep=""),width=8,height=6)
 qq.plot(x1, alpha=0.05, datatype="pvalue", scaletype="pvalue", df=1, plot.concentration.band=TRUE, one.sided=FALSE,frac=0.1, print=F, xat=NULL, yat=NULL, main=NULL, xlab=NULL, ylab=NULL, pch="x", cex=0.5, col="black")
